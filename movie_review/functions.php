b19f74899b05a8b31a5526829bd373b332f95756
abhinaychaudhary <avi9071@gmail.com>
2018-10-31 17:06:43 +0200

    function page created

---
+++ movie_review/functions.php
@@ -0,0 +1,10 @@
+<?php
+include 'db_connect.php';
+
+function generateRatings($ratings){
+    $movieRatings ='';
+    for ($i=1; $i<=$ratings; $i++){
+        $movieRatings .= '<img src="images/star.png" alt="star">';
+    }
+    return $movieRatings;
+}
