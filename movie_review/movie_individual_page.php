ad0629dc0c8d11b5089d907247a4870f33e8dc83
abhinaychaudhary <avi9071@gmail.com>
2018-10-31 17:07:13 +0200

    individual page for movie created

--- movie_review/movie_individual_page.php
+++ movie_review/movie_individual_page.php
@@ -1,10 +1,52 @@
 <?php
 include 'db_connect.php';
 include 'header.php';
+
+//select image cover
+$querySelectMovieDetails = 'select * from movies';
+$executeQuerySelectMovieDetails = mysqli_query($connect_db_movie_review,$querySelectMovieDetails) or die(mysqli_error($connect_db_movie_review));
+foreach ($executeQuerySelectMovieDetails as $value){
+
+}
+//select user profile
+$querySelectMovieCover = 'select image_id, movie_id,user_id,image_filename from images';
+$executeQuerySelectMovieCover = mysqli_query($connect_db_movie_review,$querySelectMovieCover )or die(mysqli_error($connect_db_movie_review));
+foreach ($executeQuerySelectMovieCover as $value){
+
+}
+
+//select reviews and ratings
+$querySelectReviewsAndRatings = 'select * from reviews';
+$executeQuerySelectReviewsAndRatings = mysqli_query($connect_db_movie_review,$querySelectReviewsAndRatings) or die(mysqli_error($connect_db_movie_review));
+foreach ($executeQuerySelectReviewsAndRatings as $value){
+
+}
 ?>
 <table>
-    <thead>
+    <tr>
+        <td width="450" height="550">
+            <img src="image_web/<?php echo $value['image_filename'];?>" alt="" width="450" height="550" >
+        </td>
+        <td>
+            <h1 style="float: left;">Movie Name</h1>
+            <div style="float: right">
+                Ratings:
+                <?php
+                    for ($i=1; $i<=)
+                ?>
+                <select>
+                    <option value="rate movie" selected>Rate Movie</option>
+                    <?php
+                    for ($i=1; $i<=5; $i++){
+                        echo'<option>'. $i.'</option>';
+                    }
+                    ?>
 
-    </thead>
+                </select>
+            </div>
+
+        </td>
+    </tr>
 </table>
+
 
