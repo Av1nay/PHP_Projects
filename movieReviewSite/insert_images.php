e910392d2196b5379c6225d029eeecce0c8c7c75
abhinaychaudhary <avi9071@gmail.com>
2018-11-02 15:49:46 +0200

    added and chages some files

--- movieReviewSite/insert_images.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'db_connect.php';
-?>
-<form action="validate_image.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="image" value="Upload">
-    <input type="submit" name="submit" value="Upload">
-</form>
