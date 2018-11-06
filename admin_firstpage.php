b5d0b1155d9599f944f9ebf1bd8431618dcbac38
abhinaychaudhary <avi9071@gmail.com>
2018-11-06 12:23:25 +0200

    edited item shown in admin first page

--- admin_firstpage.php
+++ admin_firstpage.php
@@ -14,24 +14,20 @@ echo '<h3 style="text-align: right;">Hello '.$_SESSION['username'].'</h3><hr>';
             <td>
                 <a href="insert_form_movietype.php">ADD GENRE</a>
             </td>
-        <tr>
-            <td>
-                <a href="insert_reviews.php">INSERT REVIEWS</a>
-            </td>
         </tr>
         <tr>
             <td>
-                <a href="movie_individual_page.php">MOVIE INDIVIDUAL PAGE</a>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <a href="insert_images.php">INSERT IMAGE</a>
+                <a href="create_user.php">CREATE USER</a>
             </td>
         </tr>
         <tr>
             <td>
                 <a href="movie_list.php">MOVIE LIST</a>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <a href="insert_in_people_form.php">INSERT PEOPLE</a>
             </td>
         </tr>
     </table>
