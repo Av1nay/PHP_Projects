169a29ae9dd1f79329988f66d046606e001015eb
abhinaychaudhary <avi9071@gmail.com>
2018-11-06 16:13:22 +0200

    edit and option action added

--- admin_firstpage.php
+++ admin_firstpage.php
@@ -3,7 +3,8 @@ session_start();
 include 'header.php';
 include 'db_connect.php';
 
-echo '<h3 style="text-align: right;">Hello '.$_SESSION['username'].'</h3><hr>';
+echo '<h3 style="text-align: right;">Hello '.$_SESSION['username'].'</h3><input type="submit" value="logout" name="logout"><hr>';
+
 ?>
 <form method="post">
     <table>
