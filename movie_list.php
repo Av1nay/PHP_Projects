ac9d070b48b6c99ed5384f18cb1303d187852c6f
abhinaychaudhary <avi9071@gmail.com>
2018-11-06 16:14:09 +0200

    list option for admin and user

--- movie_list.php
+++ movie_list.php
@@ -4,10 +4,19 @@ include 'db_connect.php';
 include 'header.php';
 
 echo '<h3 style="text-align: right;">Hello '.$_SESSION['username'].'</h3><hr>';
+echo '<table>';
 $querySelectMovies = 'select movie_id, movie_name from movies';
 $executeQuerySelectMovies = mysqli_query($connect_db_movie_review,$querySelectMovies)or die(mysqli_error($connect_db_movie_review));
-foreach ($executeQuerySelectMovies as $value){
-    $movieId =$value['movie_id'];
-    echo '<a href="movie_individual_page.php?mid='.$movieId.'">'.ucwords($value['movie_name']).'</a><br>';
+while ($row = mysqli_fetch_assoc($executeQuerySelectMovies)){
+    $movieId =$row['movie_id'];
+    if($_SESSION['username'] == 'admin'){
+        echo '<tr><td><a href="movie_individual_page.php?mid='.$movieId.'">'.ucwords($row['movie_name']).'</a></td>
+        <td><a href="#">[EDIT]</a></td><td><a href="#">[DELETE]</a></td>
+        </tr>';
+    }else{
+        echo '<tr><td><a href="movie_individual_page.php?mid='.$movieId.'">'.ucwords($row['movie_name']).'</a><td></tr>';
+    }
+    
 }
+echo '</table>';
 ?>
